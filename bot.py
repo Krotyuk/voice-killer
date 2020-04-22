e34c0375406948b060b1bd246fdf5ff83b33eb85
Krotyuk <kosonshow@gmail.com>
2020-04-22 16:57:58 +0700

    webhook correct

--- bot.py
+++ bot.py
@@ -1,33 +1,30 @@
 from aiogram import Bot, types
-
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+from aiohttp import web
+from io import BytesIO
+from aiogram.dispatcher.webhook import get_new_configured_app
+
+import os
+import ssl
 import logging
 
-from aiogram.dispatcher import Dispatcher
-from aiogram.dispatcher.webhook import SendMessage
-from aiogram.utils.executor import start_webhook
-
-from config import API_TOKEN, WEBHOOK_PORT, WEBHOOK_URL_BASE, WEBHOOK_URL_PATH, WEBHOOK_HOST, WEBHOOK_SSL_CERT
-
-from io import BytesIO
-from files_handler import save_file
-import os
-
-from to_wav import oga_to_wav_convert
 import speech_recognition as speech_recog
 
-logging.basicConfig(level=logging.INFO)
+from config import API_TOKEN, WEBHOOK_PORT, WEBHOOK_URL_BASE, WEBHOOK_URL_PATH, WEBHOOK_SSL_CERT, \
+WEBHOOK_LISTEN, WEBHOOK_SSL_PRIV
+from to_wav import oga_to_wav_convert
+from files_handler import save_file
 
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
+app = web.Application()
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nОтправь мне голосовуху, а я тебе скину текст!")
-
 
 @dp.message_handler(content_types=["voice"])
 async def voice_safe(message: types.Message):
@@ -50,44 +47,37 @@ async def voice_safe(message: types.Message):
     recog = speech_recog.Recognizer()
 
     with sample_audio as audio_file:
-         recog.adjust_for_ambient_noise(audio_file)
-         audio_content = recog.record(audio_file)
+        recog.adjust_for_ambient_noise(audio_file)
+        audio_content = recog.record(audio_file)
 
     try:
-        await message.reply(recog.recognize_google(audio_content,  language="ru-RU"))
+        await message.reply(recog.recognize_google(audio_content, language="ru-RU"))
         os.remove(wav_path)
     except Exception as e:
-        await message.reply("Непонятно( " + str(e))
+        await  message.reply("Непонятно( " + str(e))
         os.remove(wav_path)
 
 async def on_startup(dp):
     webhook = await bot.get_webhook_info()
     if webhook.url != WEBHOOK_URL_BASE:
-        # If URL doesnt match current - remove webhook
         if not webhook.url:
             await bot.delete_webhook()
-
         await bot.set_webhook(WEBHOOK_URL_BASE, certificate=open(WEBHOOK_SSL_CERT, 'rb'))
 
 async def on_shutdown(dp):
     logging.warning('Хм неполадки..')
-    # Remove webhook (not acceptable in some cases)
     await bot.delete_webhook()
-
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-
     logging.warning('Пока!')
 
-
-
 if __name__ == '__main__':
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_URL_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBHOOK_HOST,
-        port=WEBHOOK_PORT,
-    )
+
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+    web.run_app(app, host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, ssl_context=context)
